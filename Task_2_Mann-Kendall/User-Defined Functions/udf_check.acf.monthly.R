a2051d3f958f0be74cd92b5f8a1c8dee7cf707a6
milesmedina <44986194+milesmedina@users.noreply.github.com>
2022-06-01 12:10:48 -0400

    Remove unused UDFs and revision to acf script

--- Task_2_Mann-Kendall/User-Defined Functions/udf_check.acf.monthly.R
+++ Task_2_Mann-Kendall/User-Defined Functions/udf_check.acf.monthly.R
@@ -3,7 +3,7 @@
 check.acf.monthly <- function( DF ){
   
   # Check for case in which all reported values are equal (no variability in data)
-  if( length(unique( DF$Value ))==1 ){
+  if( length(unique( na.omit(DF$Value) ))==1 ){
     autocorrelated <- FALSE
     return( autocorrelated )
   }  # // end if()
@@ -12,6 +12,9 @@ check.acf.monthly <- function( DF ){
   acf.result <- acf( DF$Value, plot=F, na.action=na.pass, lag.max=12 )
   acf.6mo  <- acf.result$acf[7]
   acf.12mo <- acf.result$acf[13]
+  # If 6mo or 12mo ACF values not computed, assign zero
+  if(is.na(acf.6mo)){  acf.6mo <- 0 }
+  if(is.na(acf.12mo)){ acf.12mo <- 0 }
   # Compute critical ACF value for significance
   acf.crit <- 1.96 / sqrt(acf.result$n.used)
   # Compare 6- and 12-month ACF values to critical value
